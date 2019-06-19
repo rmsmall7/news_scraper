dc0a75ef7d03843f2a09e839c3815494deed4d6d
unknown <rmsmall7@gmail.com>
2019-06-18 22:47:48 -0400

    updated scrape.js

---
+++ models/headline.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var headLineSchema = new Schema({
+  headline: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  date: String,
+  saved: {
+    type: Boolean,
+    default: false
+  }
+});
+
+var HeadLine = mongoose.model("HeadLine", headLineSchema);
+
+module.exports = HeadLine;
